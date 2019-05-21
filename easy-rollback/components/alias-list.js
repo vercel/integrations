8ea516035a76158d7f4864247c46a7fb5e1c0f2a
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2019-05-21 06:26:48 +0530

    Allow to select aliases.

--- easy-rollback/components/alias-list.js
+++ easy-rollback/components/alias-list.js
@@ -3,7 +3,14 @@ const {htm} = require('@zeit/integration-utils');
 module.exports = ({aliases}) => htm`
 	<Box marginTop="10px">
 		<UL>
-			${aliases.map((a) => htm`<LI><Link href=${`https://${a.alias}`} target="_blank">${a.alias}</Link></LI>`)}
+			${aliases.map((a) => htm`
+				<Box display="flex" alignItems="center">
+					<Checkbox name=${`alias.${a.alias}`} checked=${true} />
+					<Box marginLeft="5px">
+						<Link href=${`https://${a.alias}`} target="_blank">${a.alias}</Link>
+					</Box>
+				</Box>
+			`)}
 		</UL>
 	</Box>
 `;
--- easy-rollback/views/rollback.js
+++ easy-rollback/views/rollback.js
@@ -1,19 +1,55 @@
 const AliasList = require('../components/alias-list');
 const {htm} = require('@zeit/integration-utils');
 
+function getSelectedAliases(clientState) {
+	const aliases = Object.keys(clientState)
+		.filter((key) => /^alias\./.test(key))
+		.filter((key) => clientState[key])
+		.map((key) => key.replace('alias.', ''));
+
+	return aliases;
+}
+
 module.exports = async function confirmView({payload, zeitClient, metadata}) {
 	const deploymentId = payload.action.replace('rollback/', '');
 	const cache = metadata.cache[payload.projectId];
 	const deployment = cache.oldDeployments.find((d) => d.id === deploymentId);
 
-	const aliasDeployment = (alias) => zeitClient.fetchAndThrow(
-		`/v2/now/deployments/${deploymentId}/aliases`,
-		{
-			method: 'POST',
-			data: {alias}
+	const aliases = getSelectedAliases(payload.clientState);
+	if (aliases.length === 0) {
+		return htm`
+			<Page>
+				<Fieldset>
+					<FsContent>
+						<H1>Aliasing Aborted</H1>
+						No alias selected for the rollback.
+					</FsContent>
+					<FsFooter>
+						<Button small action="view">Show Alias History</Button>
+					</FsFooter>
+				</Fieldset>
+			</Page>
+		`;
+	}
+
+	const aliasDeployment = async (alias) => {
+		const response = await zeitClient.fetch(
+			`/v2/now/deployments/${deploymentId}/aliases`,
+			{
+				method: 'POST',
+				data: {alias}
+			}
+		);
+
+
+		if (response.status === 200 || response.status === 409) {
+			return;
 		}
-	);
-	await Promise.all(cache.aliases.map((a) => aliasDeployment(a.alias)));
+
+		throw new Error(`failed to alias: ${alias} to deployment: ${deploymentId} - ${await response.text()}`);
+	};
+
+	await Promise.all(aliases.map((a) => aliasDeployment(a)));
 	delete metadata.cache[payload.projectId];
 	await zeitClient.setMetadata(metadata);
 
@@ -25,7 +61,7 @@ module.exports = async function confirmView({payload, zeitClient, metadata}) {
 				<FsContent>
 					<H1>Successfully Aliased</H1>
 					The deployment with url: <Link href=${url} target="_blank">${url}</Link> has aliased to:
-					<${AliasList} aliases=${cache.aliases} //>
+					<${AliasList} aliases=${aliases.map((a) => ({alias: a}))} //>
 				</FsContent>
 				<FsFooter>
 					<Button small action="view">Show Alias History</Button>

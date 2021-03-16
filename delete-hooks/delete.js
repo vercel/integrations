e3197d830cc6a8154c7acddf5e57b89e19c5dff6
Chris <7249920+chriswdmr@users.noreply.github.com>
2021-03-16 12:41:26 +0100

    Remove outdated example integrations (#154)

--- delete-hooks/delete.js
+++
@@ -1,9 +0,0 @@
-const micro = require('micro');
-
-module.exports = async function(req, res) {
-	const body = await micro.json(req);
-	console.log(`> Received the Delete Hook with the following HTTP body`);
-	console.log(JSON.stringify(body, null, 2));
-
-	micro.send(res, 200);
-}
--- webhooks-explorer/constants.js
+++
@@ -1,4 +0,0 @@
-exports.CLIENT_ID = '';
-exports.CLIENT_SECRET = '';
-exports.ROOT_URL = '';
-exports.REDIRECT_URL = 'http://localhost:5005';
